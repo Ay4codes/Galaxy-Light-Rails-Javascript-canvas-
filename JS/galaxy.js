8ec13242aa9bfad9d713a268b638f7106efef984
AyIshola <isholaolawale256@gmail.com>
2022-08-06 18:52:09 +0100

    first commit

---
+++ JS/galaxy.js
@@ -0,0 +1,115 @@
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+}
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+let mouseDown = false
+addEventListener('mousedown', () => {
+    mouseDown = true
+})
+addEventListener('mouseup', () => {
+    mouseDown = false
+})
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+
+  init()
+})
+
+// Objects
+class Particle {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.shadowColor = this.color
+    c.shadowBlur = 15
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
+  }
+
+  update() {
+    this.draw()
+  }
+}
+
+// Implementation
+let particles
+function init() {
+  particles = []
+
+  for (let i = 0; i < 600; i++) {
+    const canvasWidth = canvas.width + 400
+    const canvasHeight = canvas.height + 400
+    const x = Math.random() * canvasWidth - canvasWidth / 2
+    const y = Math.random() * canvasHeight - canvasHeight / 2
+    const radius = 2 * Math.random()
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    particles.push(new Particle(x, y, radius, color))
+  }
+}
+
+// Animation Loop
+let radians = 0
+let alpha = 1
+function animate() {
+  requestAnimationFrame(animate)
+  c.fillStyle = `rgba(4, 13, 33, ${alpha})` 
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  c.save()
+  c.translate(canvas.width / 2, canvas.height / 2)
+  c.rotate(radians)
+  particles.forEach((particle) => {
+    particle.update()
+   })
+  c.restore()
+  
+  radians += 0.005
+
+  if(mouseDown && alpha >= 0.03) {
+    alpha -= 0.01
+  } else if (!mouseDown && alpha < 1) {
+    alpha += 0.01
+  }
+}
+
+init()
+animate()
+
+
+
+
+
+
+
+// Home page Type Writer
+const myText = document.getElementById('mainMsg');
+
+var typewriter = new Typewriter(myText, {
+    loop: true
+});
+typewriter.typeString('This is a Galaxy light rails canvas effect')
+.pauseFor(2500)
+.deleteAll()
+.typeString("Click down on the page for more effect")
+.pauseFor(2500)
+.start();
